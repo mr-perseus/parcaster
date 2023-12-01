a6f31212c881705146dea9dc4be4e3935df2d700
Jan <janfrei01@gmail.com>
2023-12-01 15:17:19 +0100

    Add function build_dataframe in get_features_for_prediction.py

--- data/preprocessing/get_features_for_prediction.py
+++ data/preprocessing/get_features_for_prediction.py
@@ -1,45 +1,28 @@
-# Get features for prediction
-## Import Libraries
 import pandas as pd
-import numpy as np
-from datetime import datetime, date
-
 import openmeteo_requests
 import requests_cache
+from datetime import datetime, date, timedelta
 from retry_requests import retry
-
 from prepare_time_features import prepare_time_features
 
-## Input Timestamp
-input = "2023-12-02 12:15"
-timestmp = datetime.strptime(input, '%Y-%m-%d %H:%M')
+# Import Features
+calendar_features = pd.read_csv("raw_features_2024.csv", sep=",")
+calendar_features['date'] = pd.to_datetime(calendar_features['date'], format='%Y-%m-%d')  # Extract Date
 
-## Import Features
-calendar_features = pd.read_csv("data/preprocessing/raw_features_2024.csv", sep=",")
-calendar_features['date'] = pd.to_datetime(calendar_features['date'], format='%Y-%m-%d') # Extract Date
+# Get Weather-Data
+# Set up Client and define Params
+cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+openmeteo = openmeteo_requests.Client(session=retry_session)
 
-## Get Weather-Data
-### Set up Client and define Params
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+url = "https://api.open-meteo.com/v1/forecast"  # URL API
 
-url = "https://api.open-meteo.com/v1/forecast" # URL API 
 
-params = {
-	"latitude": 47.4239,
-	"longitude": 9.3748,
-	"daily": ["temperature_2m_max", "temperature_2m_min", "rain_sum", "snowfall_sum"],
-	"start_date": timestmp.strftime("%Y-%m-%d"),
-	"end_date": timestmp.strftime("%Y-%m-%d")
-}
-
-### Define Function
+# Define Function
 def get_weather_forecast(params):
-    
     responses = openmeteo.weather_api(url, params=params)
     response = responses[0]
-    
+
     daily = response.Daily()
     daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
     daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
@@ -47,26 +30,43 @@ def get_weather_forecast(params):
     daily_snowfall_sum = daily.Variables(3).ValuesAsNumpy()
 
     daily_data = {"date": pd.date_range(
-        start = pd.to_datetime(daily.Time(), unit = "s"),
-        end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
-        freq = pd.Timedelta(seconds = daily.Interval()),
-        inclusive = "left"
-    )}
+        start=pd.to_datetime(daily.Time(), unit="s"),
+        end=pd.to_datetime(daily.TimeEnd(), unit="s"),
+        freq=pd.Timedelta(seconds=daily.Interval()),
+        inclusive="left"
+    ), "temperature_2m_max": daily_temperature_2m_max, "temperature_2m_min": daily_temperature_2m_min,
+        "rain_sum": daily_rain_sum, "snowfall_sum": daily_snowfall_sum}
 
-    daily_data["temperature_2m_max"] = daily_temperature_2m_max
-    daily_data["temperature_2m_min"] = daily_temperature_2m_min
-    daily_data["rain_sum"] = daily_rain_sum
-    daily_data["snowfall_sum"] = daily_snowfall_sum
+    daily_dataframe = pd.DataFrame(data=daily_data)
+    return daily_dataframe
 
-    daily_dataframe = pd.DataFrame(data = daily_data)
-    return(daily_dataframe)
 
-### Get Data
-df_weather = get_weather_forecast(params)
+def build_dataframe(input_date):
+    timestamp = datetime.strptime(input_date, '%Y-%m-%d %H:%M')
 
-## Merge Weather with other Features
-df = pd.merge(df_weather, calendar_features, on="date", how="left")
-df["datetime"] = timestmp
+    params = {
+        "latitude": 47.4239,
+        "longitude": 9.3748,
+        "daily": ["temperature_2m_max", "temperature_2m_min", "rain_sum", "snowfall_sum"],
+        "start_date": timestamp.strftime("%Y-%m-%d"),
+        "end_date": timestamp.strftime("%Y-%m-%d")
+    }
 
-## Add time-features
-df = prepare_time_features(df)
+    # Get Data
+    df_weather = get_weather_forecast(params)
+
+    # Merge Weather with other Features
+    df = pd.merge(df_weather, calendar_features, on="date", how="left")
+    df["datetime"] = timestamp
+
+    # Add time-features
+    df = prepare_time_features(df)
+
+    return df
+
+
+if __name__ == "__main__":
+    date_today = date.today()
+    date_tomorrow = date_today + timedelta(days=1)
+    df_demo = build_dataframe(date_tomorrow.strftime("%Y-%m-%d %H:%M"))
+    print(df_demo.head())
--- data/preprocessing/prepare_time_features.py
+++ data/preprocessing/prepare_time_features.py
@@ -1,19 +1,20 @@
 import numpy as np
 import pandas as pd
 
+
 def prepare_time_features(df):
     # Prepare time-features
     ## Extract Time Components
-    df['datetime'] = pd.to_datetime(df['datetime'], format='%d.%m.%Y %H:%M') # Make Object to datetime
-    df['date'] = df['datetime'].dt.date # Extract Date
-    df['year'] = df['datetime'].dt.year # Extract Year
-    df['month'] = df['datetime'].dt.month # Extract Month
-    df['day'] = df['datetime'].dt.day # Extract Day
+    df['datetime'] = pd.to_datetime(df['datetime'], format='%d.%m.%Y %H:%M')  # Make Object to datetime
+    df['date'] = df['datetime'].dt.date  # Extract Date
+    df['year'] = df['datetime'].dt.year  # Extract Year
+    df['month'] = df['datetime'].dt.month  # Extract Month
+    df['day'] = df['datetime'].dt.day  # Extract Day
     df['weekdayname'] = df['datetime'].dt.day_name()
-    df['weekday'] = df['datetime'].dt.dayofweek # Extract Weekday
-    df['time'] = df['datetime'].dt.strftime('%H:%M') # Extract Time
-    df['hour'] = df['datetime'].dt.hour # Extract Hour
-    df['minute'] = df['datetime'].dt.minute # Extract Minute
+    df['weekday'] = df['datetime'].dt.dayofweek  # Extract Weekday
+    df['time'] = df['datetime'].dt.strftime('%H:%M')  # Extract Time
+    df['hour'] = df['datetime'].dt.hour  # Extract Hour
+    df['minute'] = df['datetime'].dt.minute  # Extract Minute
 
     ## Decompose Time-Features in sine and cosine component
     ### Inspired by https://medium.com/mlearning-ai/transformer-implementation-for-time-series-forecasting-a9db2db5c820 
@@ -25,15 +26,15 @@ def prepare_time_features(df):
     days_in_month = 30
     month_in_year = 12
 
-    df['sin_minute'] = np.sin(2*np.pi*df['minute']/minutes_in_hour)
-    df['cos_minute'] = np.cos(2*np.pi*df['minute']/minutes_in_hour)
-    df['sin_hour'] = np.sin(2*np.pi*df['hour']/hours_in_day)
-    df['cos_hour'] = np.cos(2*np.pi*df['hour']/hours_in_day)
-    df['sin_weekday'] = np.sin(2*np.pi*df['weekday']/days_in_week)
-    df['cos_weekday'] = np.cos(2*np.pi*df['weekday']/days_in_week)
-    df['sin_day'] = np.sin(2*np.pi*df['day']/days_in_month)
-    df['cos_day'] = np.cos(2*np.pi*df['day']/days_in_month)
-    df['sin_month'] = np.sin(2*np.pi*df['month']/month_in_year)
-    df['cos_month'] = np.cos(2*np.pi*df['month']/month_in_year)
-    
-    return df 
+    df['sin_minute'] = np.sin(2 * np.pi * df['minute'] / minutes_in_hour)
+    df['cos_minute'] = np.cos(2 * np.pi * df['minute'] / minutes_in_hour)
+    df['sin_hour'] = np.sin(2 * np.pi * df['hour'] / hours_in_day)
+    df['cos_hour'] = np.cos(2 * np.pi * df['hour'] / hours_in_day)
+    df['sin_weekday'] = np.sin(2 * np.pi * df['weekday'] / days_in_week)
+    df['cos_weekday'] = np.cos(2 * np.pi * df['weekday'] / days_in_week)
+    df['sin_day'] = np.sin(2 * np.pi * df['day'] / days_in_month)
+    df['cos_day'] = np.cos(2 * np.pi * df['day'] / days_in_month)
+    df['sin_month'] = np.sin(2 * np.pi * df['month'] / month_in_year)
+    df['cos_month'] = np.cos(2 * np.pi * df['month'] / month_in_year)
+
+    return df
