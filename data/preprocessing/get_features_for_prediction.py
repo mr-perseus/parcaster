8cc356e7666e956967c0d964e9307d21b914c978
ritahaffter <rita_haffter@hotmail.com>
2023-12-01 14:05:50 +0100

    Prep Featurea

--- data/preprocessing/get_features_for_prediction.py
+++ data/preprocessing/get_features_for_prediction.py
@@ -8,12 +8,14 @@ import openmeteo_requests
 import requests_cache
 from retry_requests import retry
 
+from prepare_time_features import prepare_time_features
+
 ## Input Timestamp
 input = "2023-12-02 12:15"
 timestmp = datetime.strptime(input, '%Y-%m-%d %H:%M')
 
 ## Import Features
-calendar_features = pd.read_csv("raw_features_2024.csv", sep=",")
+calendar_features = pd.read_csv("data/preprocessing/raw_features_2024.csv", sep=",")
 calendar_features['date'] = pd.to_datetime(calendar_features['date'], format='%Y-%m-%d') # Extract Date
 
 ## Get Weather-Data
@@ -22,7 +24,7 @@ cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
-url = "https://api.open-meteo.com/v1/forecast" # URL API
+url = "https://api.open-meteo.com/v1/forecast" # URL API 
 
 params = {
 	"latitude": 47.4239,
@@ -67,5 +69,4 @@ df = pd.merge(df_weather, calendar_features, on="date", how="left")
 df["datetime"] = timestmp
 
 ## Add time-features
-with open("prepare_time_features.py") as f:
-    exec(f.read())
+df = prepare_time_features(df)
